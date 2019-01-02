693eb7f15b441f502dc5a55432081d8d4c05208a
ShmatEugene <shmat.evg@yandex.ru>
2019-01-02 23:59:11 +0300

    Theatre page + home bg animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
+const defaultBg = 'url(img/home-default-bg.jpg)';
+
+function changeBg(attribute, defaultBgSelector, imgIdInUrl, startOfTheUrl) {
+  console.log(attribute);
+  let imgUrl = startOfTheUrl + attribute.attr('id').slice(imgIdInUrl) + '.jpg)';
+  console.log(imgUrl);
+  $(defaultBgSelector).addClass('home-new-bg');
+  $('.home-new-bg').css('background-image', imgUrl);
+}
+
 $('.home-theatre-block').hover(function() {
-  $('.home-default-bg').addClass('h-t-b-img-1');
+  changeBg($(this), '.home-default-bg', -1, 'url(img/h');
 }, function() {
-  $('.home-default-bg').removeClass('h-t-b-img-1');
+  $('.home-new-bg').css('background-image', defaultBg);
+})
+
+
+$('.events-slider-controller').hover(function() {
+  var urlStart = 'url(img/' + $(this).attr('id').slice(0, 2) + '/';
+  changeBg($(this), '.event-slide-bg', -1, urlStart);
+  //$('.events-target1-date').addClass('event-date-active');
+  //let dateSelector = '.events-target' + $(this).attr('id').slice(-1) + 'date';
+  //$(dateSelector).addClass('event-date-active');
+  $('.event-slide-date-text').removeClass('event-date-active')
+  let dateId = $(this).attr('id').slice(-1);
+  if( dateId == 1) {
+    $('.events-target1-date').addClass('event-date-active');
+  } else if(dateId == 2) {
+    $('.events-target2-date').addClass('event-date-active');
+  } else if(dateId == 3) {
+    $('.events-target3-date').addClass('event-date-active');
+  }  else if(dateId == 4) {
+    $('.events-target4-date').addClass('event-date-active');
+  }
+  console.log('.events-target' + $(this).attr('id').slice(-1) + '-date');
+}, function() {
+
 })
